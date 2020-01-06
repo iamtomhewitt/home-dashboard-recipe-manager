eecee9f4da95878e51c4007174946646bc8f398d
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 13:18:28 +0000

    💻 Update planner routes with an optional day query

--- routes/planner-routes.js
+++ routes/planner-routes.js
@@ -10,17 +10,37 @@ var plannerFilename = path.join(__dirname, '..', 'data', 'planner.json');
 
 router.get('/', function (req, res) {
 	let response;
+	let day = req.query.day;
 
 	if (!fs.existsSync(plannerFilename)) {
 		response = error(`${plannerFilename} does not exist`);
 		res.status(errorCode).send(response).json();
 		return;
 	}
-
+	
 	let contents = fs.readFileSync(plannerFilename, 'utf-8');
-	response = success('Success')
-	response['planner'] = JSON.parse(contents)['planner'];
-	res.status(successCode).send(response);
+
+	if (day) {
+		let days = JSON.parse(contents)['planner'];
+	
+		const found = days.find(x => x.day === day);
+		
+		if (!found) {
+			response = error(`${day} not a valid day`)
+			res.status(errorCode).send(response);
+			return;
+		}
+
+		response = success(`Found plan for '${day}'`);
+		response['planner'] = found;
+		res.status(successCode).send(response);
+	}
+
+	else {
+		response = success('Success')
+		response['planner'] = JSON.parse(contents)['planner'];
+		res.status(successCode).send(response);
+	}
 });
 
 router.get('/add', function (req, res) {
