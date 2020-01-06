1e24d06d0bb65a23e2116070b496c1a70d1e4a59
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 13:10:03 +0000

    💻 Add /planner/get

--- routes/planner-routes.js
+++ routes/planner-routes.js
@@ -9,9 +9,22 @@ var errorCode = 502;
 var plannerFilename = path.join(__dirname, '..', 'data', 'planner.json');
 
 router.get('/', function (req, res) {
+	let response;
+
+	if (!fs.existsSync(plannerFilename)) {
+		response = error(`${plannerFilename} does not exist`);
+		res.status(errorCode).send(response).json();
+		return;
+	}
+
+	let contents = fs.readFileSync(plannerFilename, 'utf-8');
+	response = success('Success')
+	response['planner'] = JSON.parse(contents)['planner'];
+	res.status(successCode).send(response);
 });
 
 router.get('/add', function (req, res) {
+
 });
 
 function success(message) {
