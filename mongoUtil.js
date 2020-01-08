f5b8aff090dd6a5efebea22b98c84562d405347e
Tom Hewitt <tommhewitt@gmail.com>
2020-01-08 16:35:19 +0000

    💻 Create planner collection if it does not exist

--- mongoUtil.js
+++ mongoUtil.js
@@ -11,7 +11,40 @@ module.exports = {
         mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
             db = client.db(databaseName);
             db.createCollection('recipes');
-            db.createCollection('planner');
+
+            // Create collections
+            db.collection('planner').updateOne({ planner: { $exists: true } }, {$set:{
+                planner: [
+                    {
+                        day: 'Monday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Tuesday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Wednesday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Thursday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Friday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Saturday',
+                        recipe: '',
+                    },
+                    {
+                        day: 'Sunday',
+                        recipe: '',
+                    },
+                ],
+            }}, { upsert: true });
             return callback(err);
         });
     },
