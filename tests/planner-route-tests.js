49109388e0988d7dd29e985b2c8eb86af2220f53
Tom Hewitt <tommhewitt@gmail.com>
2020-01-09 14:01:19 +0000

    🧪 Check the response messages in /planner tests

--- tests/planner-route-tests.js
+++ tests/planner-route-tests.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const assert = require('assert');
 
 describe('/planner tests', () => {
     let server;
@@ -14,19 +15,45 @@ describe('/planner tests', () => {
     it('/planner should give 200', (done) => {
         request(server)
             .get('/planner')
-            .expect(200, done);
+            .expect(200)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.notEqual(response.body.planner, undefined);
+
+                return done();
+            });
     });
 
     it('/planner?day=Monday should give 200', (done) => {
         request(server)
             .get('/planner?day=Monday')
-            .expect(200, done);
+            .expect(200)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.planner.day, 'Monday');
+                assert.equal(response.body.message, 'Success');
+                return done();
+            });
     });
 
     it('/planner?day=invalid should give 502', (done) => {
         request(server)
             .get('/planner?day=invalid')
-            .expect(502, done);
+            .expect(502)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.message, 'Could not get planner: \'invalid\' not a valid day');
+                return done();
+            });
     });
 });
 
@@ -48,13 +75,29 @@ describe('/planner/add tests', () => {
                 day: 'Monday',
                 recipe: 'Some recipe',
             })
-            .expect(200, done);
+            .expect(200)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.message, "Recipe 'Some recipe' added");
+                return done();
+            });
     });
 
     it('/planner/add should give 502', (done) => {
         request(server)
             .post('/planner/add')
-            .expect(502, done);
+            .expect(502)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.message, 'Planner could not be updated, missing data from JSON body. Expected: {"day":"<day>","recipe":"<recipe>"} Got: {}');
+                return done();
+            });
     });
 
     it('/planner/add with JSON payload and incorrect day should give 502', (done) => {
@@ -64,6 +107,14 @@ describe('/planner/add tests', () => {
                 day: 'MadeupDay',
                 recipe: 'Some recipe',
             })
-            .expect(502, done);
+            .expect(502)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.message, "Planner could not be updated: 'MadeupDay' not a valid day");
+                return done();
+            });
     });
 });
