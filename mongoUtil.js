0bff701b3202aa22b2bdbba0eb20501cad9005cd
Tom Hewitt <tommhewitt@gmail.com>
2020-01-09 09:32:35 +0000

    💻 Get mongo connecting

--- mongoUtil.js
+++ mongoUtil.js
@@ -1,58 +1,56 @@
 const mongo = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost:27017';
-const databaseName = 'recipe_manager';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipe_manager';
 
 let db;
 
 module.exports = {
 
     connectToServer(callback) {
-        const urlToUse = process.env.MONGODB_URI || url;
-        const databaseNameToUse = process.env.MONGODB_DATABASE_NAME || databaseName;
+        mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Connected to: ${url}`);
+                db = client.db();
 
-        console.log(urlToUse);
-        console.log(databaseNameToUse);
-
-        mongo.connect(urlToUse, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-            db = client.db(databaseNameToUse);
-
-            // Create collections
-            db.createCollection('recipes');
-            db.collection('planner').updateOne({ planner: { $exists: true } }, {
-                $set: {
-                    planner: [
-                        {
-                            day: 'Monday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Tuesday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Wednesday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Thursday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Friday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Saturday',
-                            recipe: '',
-                        },
-                        {
-                            day: 'Sunday',
-                            recipe: '',
-                        },
-                    ],
-                },
-            }, { upsert: true });
+                // Create collections
+                db.createCollection('recipes');
+                db.collection('planner').updateOne({ planner: { $exists: true } }, {
+                    $set: {
+                        planner: [
+                            {
+                                day: 'Monday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Tuesday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Wednesday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Thursday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Friday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Saturday',
+                                recipe: '',
+                            },
+                            {
+                                day: 'Sunday',
+                                recipe: '',
+                            },
+                        ],
+                    },
+                }, { upsert: true });
+            }
             return callback(err);
         });
     },
