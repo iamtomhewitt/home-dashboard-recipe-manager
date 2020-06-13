e679a5f52d3cad5990ca15d9e48ece62ea7694c1
Tom Hewitt <tommhewitt@gmail.com>
2020-06-13 22:35:26 +0100

    refactor(tests): update request test data

--- tests/planner/responseData.js
+++ tests/planner/responseData.js
@@ -1,9 +1,6 @@
 const { BAD_REQUEST, UNAUTHORISED } = require('../../responses/codes');
 
 module.exports = {
-  PLANNER_ENTRY_CREATED: {
-    message: 'Recipe \'Some recipe\' added',
-  },
 
   PLANNER_ENTRY: {
     planner: {
@@ -11,12 +8,6 @@ module.exports = {
       recipe: 'Some recipe',
     },
     message: 'Success',
-  },
-
-  PLANNER_BAD_REQUEST(expectedJson) {
-    return {
-      message: expectedJson,
-    };
   },
 
   PLANNER_NO_API_KEY: {
@@ -27,10 +18,6 @@ module.exports = {
   PLANNER_API_KEY_INCORRECT: {
     code: UNAUTHORISED,
     message: 'API key is incorrect',
-  },
-
-  PLANNER_ADD_INVALID_DAY: {
-    message: 'Planner could not be updated: \'MadeupDay\' not a valid day',
   },
 
   PLANNER_GET_INVALID_DAY: {
