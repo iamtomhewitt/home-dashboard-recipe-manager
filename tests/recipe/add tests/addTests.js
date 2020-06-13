ac3a3825279a3dfb435c737945eeb2bbf09b0c5a
Tom Hewitt <tommhewitt@gmail.com>
2020-06-13 21:56:40 +0100

    refactor(tests): update add recipe tests

--- tests/recipe/add tests/addTests.js
+++ tests/recipe/add tests/addTests.js
@@ -1,9 +1,12 @@
 const request = require('supertest');
 const assert = require('assert');
-const { BODY_ADD_RECIPE, BODY_ADD_RECIPE_MISSING_INGREDIENTS } = require('./testData');
+const { BODY_ADD_RECIPE, BODY_ADD_RECIPE_MISSING_INGREDIENTS } = require('../testData');
 const {
   BAD_REQUEST, UNAUTHORISED, CREATED, SERVER_ERROR,
-} = require('../../responses/codes');
+} = require('../../../responses/codes');
+const {
+  RECIPE_CREATED, RECIPE_BAD_REQUEST, RECIPE_EXISTS, RECIPE_API_KEY_INCORRECT, RECIPE_NO_API_KEY,
+} = require('./responseData');
 
 const ROUTE = '/recipes/add';
 require('dotenv').config();
@@ -12,7 +15,7 @@ describe('Add recipe tests', () => {
   let server;
 
   before(() => {
-    server = require('../../app').listen(3002);
+    server = require('../../../app').listen(3002);
   });
 
   after(() => {
@@ -29,7 +32,7 @@ describe('Add recipe tests', () => {
           return done(err);
         }
 
-        assert.equal(response.body.message, "Recipe 'New' added");
+        assert.deepEqual(response.body, RECIPE_CREATED);
         return done();
       });
   });
@@ -46,7 +49,7 @@ describe('Add recipe tests', () => {
 
         // eslint-disable-next-line max-len
         const expectedMessage = `Recipe could not be added, missing data from JSON body. Expected: {"name":"<recipe name>","serves":"<optional>","ingredients":[{"name":"<name>","category":"<category>","amount":"<amount>","weight":"<weight>"}],"steps":["1. Example","2. Example"]} Got: {"name":"Missing","serves":1,"ingredients":[{"name":"pepper"},{"name":"chicken"}],"steps":["1. Do something","2. Do something else"],"apiKey":"${process.env.API_KEY}"}`;
-        assert.equal(response.body.message, expectedMessage);
+        assert.deepEqual(response.body, RECIPE_BAD_REQUEST(expectedMessage));
         return done();
       });
   });
@@ -61,7 +64,7 @@ describe('Add recipe tests', () => {
           return done(err);
         }
 
-        assert.equal(response.body.message, "Cannot add recipe: 'New' already exists");
+        assert.deepEqual(response.body, RECIPE_EXISTS);
         return done();
       });
   });
@@ -69,9 +72,7 @@ describe('Add recipe tests', () => {
   it('should give bad request when missing data in request body', (done) => {
     request(server)
       .post(ROUTE)
-      .send({
-        apiKey: process.env.API_KEY,
-      })
+      .send({ apiKey: process.env.API_KEY })
       .expect(BAD_REQUEST)
       .end((err, response) => {
         if (err) {
@@ -80,7 +81,7 @@ describe('Add recipe tests', () => {
 
         // eslint-disable-next-line max-len
         const expectedMessage = `Recipe could not be added, missing data from JSON body. Expected: {"name":"<recipe name>","serves":"<optional>","ingredients":[{"name":"<name>","category":"<category>","amount":"<amount>","weight":"<weight>"}],"steps":["1. Example","2. Example"]} Got: {"apiKey":"${process.env.API_KEY}"}`;
-        assert.equal(response.body.message, expectedMessage);
+        assert.deepEqual(response.body, RECIPE_BAD_REQUEST(expectedMessage));
         return done();
       });
   });
@@ -88,16 +89,14 @@ describe('Add recipe tests', () => {
   it('should give unauthorised if api key is incorrect', (done) => {
     request(server)
       .post(ROUTE)
-      .send({
-        apiKey: 'incorrect',
-      })
+      .send({ apiKey: 'incorrect' })
       .expect(UNAUTHORISED)
       .end((err, response) => {
         if (err) {
           return done(err);
         }
 
-        assert.equal(response.body.message, 'API key is incorrect');
+        assert.deepEqual(response.body, RECIPE_API_KEY_INCORRECT);
         return done();
       });
   });
@@ -112,7 +111,7 @@ describe('Add recipe tests', () => {
           return done(err);
         }
 
-        assert.equal(response.body.message, 'No API key specified');
+        assert.deepEqual(response.body, RECIPE_NO_API_KEY);
         return done();
       });
   });
