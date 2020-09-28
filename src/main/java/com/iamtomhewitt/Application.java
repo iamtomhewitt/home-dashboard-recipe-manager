fc23806de120555503419e1c1d5714a6550682c9
Tom Hewitt <tommhewitt@gmail.com>
2020-09-28 20:59:32 +0100

    chore(#28): refactor files

--- src/main/java/com/iamtomhewitt/Application.java
+++ src/main/java/com/iamtomhewitt/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeDashboardRecipeManagerApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(HomeDashboardRecipeManagerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
