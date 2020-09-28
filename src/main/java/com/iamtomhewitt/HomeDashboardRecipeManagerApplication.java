dde481063b5863f587f649c6d73ce09262e7bd4b
Tom Hewitt <tommhewitt@gmail.com>
2020-09-28 20:55:59 +0100

    feat(#28): initialise spring application

---
+++ src/main/java/com/iamtomhewitt/HomeDashboardRecipeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iamtomhewitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDashboardRecipeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDashboardRecipeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamtomhewitt/HomeDashboardRecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamtomhewitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeDashboardRecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
