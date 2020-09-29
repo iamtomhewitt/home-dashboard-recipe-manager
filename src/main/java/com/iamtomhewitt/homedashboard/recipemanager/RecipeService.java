bba0f81c7662d2d5effc154b05964d65d0ee81e7
Tom Hewitt <tommhewitt@gmail.com>
2020-09-29 09:31:26 +0100

    chore(#28): implement finding recipes

--- src/main/java/com/iamtomhewitt/homedashboard/recipemanager/RecipeService.java
+++ src/main/java/com/iamtomhewitt/homedashboard/recipemanager/RecipeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -13,6 +14,9 @@ public class RecipeService {
 	@Autowired
 	private RecipeRepository repository;
 
+	public Recipe getRecipe(String name) throws RecipeNotFoundException {
+		return Optional.ofNullable(repository.findByName(name)).orElseThrow(() -> new RecipeNotFoundException(name));
+	}
 	public List<Recipe> getRecipes() {
 		return repository.findAll();
 	}
--- src/main/java/com/iamtomhewitt/homedashboard/recipemanager/controller/RecipeController.java
+++ src/main/java/com/iamtomhewitt/homedashboard/recipemanager/controller/RecipeController.java
@@ -1,10 +1,12 @@
 package com.iamtomhewitt.homedashboard.recipemanager.controller;
 
+import com.iamtomhewitt.homedashboard.recipemanager.RecipeNotFoundException;
 import com.iamtomhewitt.homedashboard.recipemanager.RecipeService;
 import com.iamtomhewitt.homedashboard.recipemanager.model.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +17,11 @@ public class RecipeController {
 
 	@Autowired
 	private RecipeService service;
+
+	@GetMapping(params = "name")
+	public Recipe getRecipe(@RequestParam String name) throws RecipeNotFoundException {
+		return this.service.getRecipe(name);
+	}
 
 	@GetMapping
 	public List<Recipe> getRecipes() {
