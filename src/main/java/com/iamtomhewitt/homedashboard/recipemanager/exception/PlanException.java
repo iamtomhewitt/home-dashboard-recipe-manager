669aaab452f14b79fc27c100f3fdaa4c21194597
Tom Hewitt <tommhewitt@gmail.com>
2020-10-06 21:06:09 +0100

    feat(#32): add route for getting a plan by day

---
+++ src/main/java/com/iamtomhewitt/homedashboard/recipemanager/exception/PlanException.java
@@ -0,0 +1,8 @@
+package com.iamtomhewitt.homedashboard.recipemanager.exception;
+
+public class PlanException extends Exception {
+
+	public PlanException(String message) {
+		super(message);
+	}
+}
