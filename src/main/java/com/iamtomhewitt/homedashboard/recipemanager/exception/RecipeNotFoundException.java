e5e641b02e121c454df8f93f923a03118d4d3e83
Tom Hewitt <tom.hewitt@williamhill.com>
2021-08-29 15:23:16 +0100

    chore(#50): remove java code

--- src/main/java/com/iamtomhewitt/homedashboard/recipemanager/exception/RecipeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.iamtomhewitt.homedashboard.recipemanager.exception;
-
-public class RecipeNotFoundException extends Exception {
-
-	public RecipeNotFoundException(String name) {
-		super(String.format("Could not find recipe with name '%s'", name));
-	}
-}
