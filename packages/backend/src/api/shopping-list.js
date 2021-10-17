32b00f272dd88f2c960b8a507550a88500407633
Tom Hewitt <tom.hewitt@williamhill.com>
2021-10-17 19:51:17 +0100

    fix(#59): shopping list doesnt add ingredients together correctly

--- packages/backend/src/api/shopping-list.js
+++ packages/backend/src/api/shopping-list.js
@@ -4,8 +4,12 @@ const service = require('../services/shopping-list');
 const route = express.Router();
 
 route.get('/', async (req, res) => {
-  const shoppingList = await service.getShoppingList(req.query.id);
-  return res.status(200).json(shoppingList);
+  try {
+    const shoppingList = await service.getShoppingList(req.query.id);
+    return res.status(200).json(shoppingList);
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
 });
 
 module.exports = route;
