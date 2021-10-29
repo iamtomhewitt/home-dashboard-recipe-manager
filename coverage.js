5d1ee62676886ed98b32e05a3685c41c8468a668
Tom Hewitt <32467948+iamtomhewitt@users.noreply.github.com>
2021-10-29 08:26:34 +0100

    chore(#63): use new github-releaser for updating sub packages

--- coverage.js
+++ coverage.js
@@ -1,17 +1,18 @@
 // Combines coverage reports
 const fs = require('fs');
-const reports = ['/packages/backend/coverage.txt', '/packages/frontend/coverage.txt']
+const reports = ['/packages/backend/coverage.txt', '/packages/frontend/coverage.txt'];
 let coverages = 0;
 
 const run = async () => {
   await reports.forEach(async (report) => {
-    const contents = await fs.readFileSync(process.cwd() + report, 'utf-8')
+    const contents = await fs.readFileSync(process.cwd() + report, 'utf-8');
     const result = contents.match(/All\sfiles.*?\s+(.\d\d.\d\d)/);
-    const coverage = result[1].trim()
-    coverages += parseFloat(coverage)
+    const coverage = result[1].trim();
+    coverages += parseFloat(coverage);
   })
 
-  console.log(coverages / reports.length)
+  const finalCoverage = parseFloat(coverages / reports.length).toFixed(2);
+  console.log(finalCoverage);
 }
 
 run();
