96a0504b8e468041769e8ad17e18e14d13b8f877
Tom Hewitt <tommhewitt@gmail.com>
2021-11-07 15:57:26 +0000

    feat(#65): single save button on the planner

--- packages/frontend/src/components/Planner/index.spec.js
+++ packages/frontend/src/components/Planner/index.spec.js
@@ -20,22 +20,22 @@ describe('<Planner/>', () => {
   it('should save when updating planner', async () => {
     jest.spyOn(http, 'put').mockResolvedValue({ message: 'test message' });
 
-    const keyDownEvent = { key: 'ArrowDown', };
-    const firstRecipe = 'Food Monday'
-    const newRecipe = recipes[0].name
+    const keyDownEvent = { key: 'ArrowDown' };
+    const firstRecipe = 'Food Monday';
+    const newRecipe = recipes[0].name;
 
     const selectOption = async (container, optionText) => {
       const placeholder = getByText(container, firstRecipe);
       fireEvent.keyDown(placeholder, keyDownEvent);
       await findByText(container, optionText);
       fireEvent.click(getByText(container, optionText));
-    }
+    };
 
     const { getAllByTestId, getByTestId } = render(<Planner {...props} />);
-    const firstRecipePlan = getAllByTestId('planner-recipe')[0]
-    await selectOption(firstRecipePlan, newRecipe)
+    const firstRecipePlan = getAllByTestId('planner-recipe')[0];
+    await selectOption(firstRecipePlan, newRecipe);
 
-    const saveButton = getByTestId('planner-save-button')
+    const saveButton = getByTestId('planner-save-button');
     fireEvent.click(saveButton);
 
     expect(http.put).toHaveBeenCalledWith('/planner?id=12345', { day: 'Monday', recipe: 'MOCK Bacon And Leek Risotto' });
