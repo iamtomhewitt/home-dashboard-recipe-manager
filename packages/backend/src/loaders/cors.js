084b2333fc2f4d80777cb3b4f738e0208ac96474
Tom Hewitt <tom.hewitt@williamhill.com>
2022-09-26 11:34:54 +0100

    chore(#84): tidy package

--- packages/backend/src/loaders/cors.js
+++
@@ -1,5 +0,0 @@
-const cors = require('cors');
-
-module.exports = async (app) => {
-  app.use(cors());
-};
--- packages/backend/src/loaders/index.js
+++
@@ -1,7 +0,0 @@
-const expressLoader = require('./express');
-const corsLoader = require('./cors');
-
-module.exports = async (app) => {
-  await corsLoader(app);
-  await expressLoader(app);
-};
