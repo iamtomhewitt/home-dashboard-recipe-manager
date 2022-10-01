6b24c98adc50e999b59b100cc09cc4a99f2fa7fa
Tom Hewitt <tom.hewitt@williamhill.com>
2022-10-01 10:35:35 +0100

    chore(#84): deployment

--- packages/frontend/src/components/Ingredient/index.js
+++ packages/frontend/src/components/Ingredient/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactSelect from 'react-select';
+import ReactSelect from 'react-dropdown';
 
 import CloseIcon from '../CloseIcon';
 import './index.scss';
